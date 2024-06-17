cd62b5c5a177911cb9614f11a337d75b768888c2
lasercata <67599917+lasercata@users.noreply.github.com>
2024-06-17 10:35:12 +0200

    Adding a script to make queries

---
+++ post_a_query.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script takes a .cypher or .json file in input, and post a query to the database with the content of the file.
+
+if [[ -e $1 ]]; then
+    if [ "${1: -5}" == ".json" ]; then
+        curl localhost:3000/findPattern -X POST -H 'Content-Type: application/json' -d @"$1" | jq
+    elif [ "${1: -7}" == ".cypher" ]; then
+        json_content="{\"string\": \"$(cat $1 | sed -z 's/\n/ /g')\"}";
+        curl localhost:3000/findPattern -X POST -H 'Content-Type: application/json' -d "$json_content" | jq
+    else
+        echo Expected a .json or .cypher file !
+    fi
+elif [[ -z $1 ]]; then
+    echo Usage: $0 filename
+    echo where filename is a .json or .cypher file.
+else
+    echo File \"$1\" not found !
+fi
