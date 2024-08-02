719ed54ce2e2a584376e8bfbc7fd05b834ad630b
lasercata <67599917+lasercata@users.noreply.github.com>
2024-08-02 11:06:28 +0200

    Improving `loadAllDB.sh`: now check/ask pwd, + more general

--- loadAllDB.sh
+++ loadAllDB.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
-cypher-shell -u neo4j -p root -d neo4j --format verbose "match (n) detach delete n;"
-cypher-shell -u neo4j -p root -d neo4j --format verbose "match (n) return count(distinct n.inputfile);"
-echo "********************";
-echo "Loading Francois-Marie-Luzel..."
-cypher-shell -u neo4j -p root -d neo4j --format verbose -f /home/shaman/Public/DSL/SKRIDPlatform/assets/data/Francois-Marie-Luzel/cmdPopulate_Elaine.cql;
-cypher-shell -u neo4j -p root -d neo4j --format verbose "match (n) where n.collection='Francois-Marie Luzel' return count(distinct n.inputfile);"
-echo "********************";
-echo "Loading Joseph-Mahe-Original..."
-cypher-shell -u neo4j -p root -d neo4j --format verbose -f /home/shaman/Public/DSL/SKRIDPlatform/assets/data/Joseph-Mahe-Original/cmdPopulate_Elaine.cql;
-cypher-shell -u neo4j -p root -d neo4j --format verbose "match (n) where n.collection='Joseph Mahe Original' return count(distinct n.inputfile);"
-echo "********************";
-echo "Loading /Joseph-Mahe-TransposedG..."
-cypher-shell -u neo4j -p root -d neo4j --format verbose -f /home/shaman/Public/DSL/SKRIDPlatform/assets/data/Joseph-Mahe-TransposedG/cmdPopulate_Elaine.cql;
-cypher-shell -u neo4j -p root -d neo4j --format verbose "match (n) where n.collection='Joseph Mahe TransposedG' return count(distinct n.inputfile);"
+
+#------Init
+user=neo4j
+database=neo4j
+
+password_file=.database_password
+
+if [ -f $password_file ]; then
+    password=$(cat $password_file)
+else
+    # Read Password
+    echo -n "Database password (will be written to $password_file) :" 
+    read -s password
+
+    echo $password > $password_file
+fi
+
+#------Generate database
+cd assets/data/
+make
+
+#------Populate database
+#---Clear the current database
+echo "*************************";
+echo "Clearing the database ..."
+echo "*************************";
+cypher-shell -u $user -p $password -d $database --format verbose "match (n) detach delete n;"
+# cypher-shell -u $user -p $password -d $database --format verbose "match (n) return count(distinct n.inputfile);"
+
+#---Load all
+for d in */; do
+    if [[ "$d" != "Musyphjer/" ]]; then
+        echo "*****************************";
+        echo "Loading the database '$d' ..."
+        echo "*****************************";
+
+        cypher-shell -u $user -p $password -d $database --format verbose -f $d/load_DB.cql
+
+        echo "Done !"
+    fi
+done
+
